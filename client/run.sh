449b30d46da8dfc7188676202ae1b55d16285446
wangdayong228 <wangdayong228@163.com>
2026-01-19 11:13:21 +0800

    feat: export json after deploy l1 contracts

--- client/run.sh
+++ client/run.sh
@@ -1,5 +1,5 @@
-export SSH_KEY_PATH="$HOME/.ssh/4node-test.pem"
-export IPS="16.52.84.122 3.98.164.64 16.52.149.43 3.97.182.183"
+export SSH_KEY_PATH="$HOME/.ssh/dayong-op-stack.pem"
+export IPS="16.148.40.120 44.245.157.194 18.236.251.160 34.221.221.7"
 export L1_ESPACE_RPC_URL="ws://47.243.70.39/ws"
 # 用于L1发送交易的管理员地址私钥
 export L1_ADMIN_PRIVATE_KEY=0xd01fd3d7fdcc808840d676f4cbff81af45b2641d414d7a00e25c7bf8cc6c7e97
